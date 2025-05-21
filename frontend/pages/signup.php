e75a4c162527f3a686cfa53a3c9f2520cd249bb0
SharafALmikhlafi <sharafalmekhlafi1@gmail.com>
2025-05-21 16:56:56 +0300

    fix login direciont

--- frontend/pages/signup.php
+++ frontend/pages/signup.php
@@ -85,7 +85,7 @@ require_once '../../config.php';
                 </form>
                 <div class="text-center pt-2">
                     <span>لدي حساب بالفعل، </span>
-                    <a href="frontend/pages/login.php">تسجيل الدخول</a>
+                    <a href="login.php">تسجيل الدخول</a>
                 </div>
                 <div id="errorMsg" class="alert alert-danger d-none" role="alert"></div>
             </div>
